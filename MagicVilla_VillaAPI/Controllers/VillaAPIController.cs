25196f2eade52df048d0cd6d12aacbd17bcdf4b0
ali-alzaiat <alialzaiat2000@gmail.com>
2024-09-04 15:19:02 +0300

    Created VillaAPI controller.

---
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+    }
+}
